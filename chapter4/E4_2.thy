e94602cb8a99f473c7e2cd8a5ab6e91ab0f59c86
mu001999 <mu001999@outlook.com>
2020-03-12 13:19:53 +0800

    update e-2

--- chapter4/E4_2.thy
+++ chapter4/E4_2.thy
@@ -4,20 +4,16 @@ begin
 
 lemma "∃ ys zs. xs = ys @ zs ∧ (length ys = length zs ∨ length ys = length zs + 1)"
 proof cases
-  let ?l1 = " take ((length xs div 2)) xs"
-  let ?l2 = " drop ((length xs div 2)) xs"
+  let ?l1 = "take ((length xs div 2)) xs"
+  let ?l2 = "drop ((length xs div 2)) xs"
   assume "even (length xs)"
-  have "xs = ?l1 @ ?l2" by auto
-  hence "length ?l1 = length ?l2" using ‹even (length xs)› by fastforce
   hence "xs = ?l1 @ ?l2 ∧ length ?l1 = length ?l2" by auto
   thus ?thesis by blast
 next
-  let ?l1 = " take ((length xs div 2) + 1) xs"
-  let ?l2 = " drop ((length xs div 2) + 1) xs"
+  let ?l1 = "take ((length xs div 2) + 1) xs"
+  let ?l2 = "drop ((length xs div 2) + 1) xs"
   assume "odd (length xs)"
-  have "xs = ?l1 @ ?l2" by auto
-  hence "length ?l1 = length ?l2 + 1" by (smt ‹odd (length xs)› add.commute add_diff_cancel_left' left_add_twice length_append length_drop odd_two_times_div_two_succ)
-  hence "xs = ?l1 @ ?l2 ∧ length ?l1 = length ?l2 + 1" by auto
+  hence "xs = ?l1 @ ?l2 ∧ length ?l1 = length ?l2 + 1" by (smt add.commute add_diff_cancel_right' append_take_drop_id left_add_twice length_append length_drop odd_two_times_div_two_succ)
   thus ?thesis by blast
 qed
 
